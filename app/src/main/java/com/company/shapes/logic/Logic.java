de770f953fb8ce67c304ce8514d5c65f4a2055dd
ErvinApk I-2-21 <66515174+Erwin503@users.noreply.github.com>
2024-02-28 13:47:04 +0300

    final

--- app/src/main/java/com/company/shapes/logic/Logic.java
+++ app/src/main/java/com/company/shapes/logic/Logic.java
@@ -1,6 +1,6 @@
-package com.company.shapes.logic;
+package com.company.diamonds.logic;
 
-import com.company.shapes.ui.OutputInterface;
+import com.company.diamonds.ui.OutputInterface;
 
 /**
  * This is where the logic of this App is centralized for this assignment.
@@ -9,42 +9,31 @@ import com.company.shapes.ui.OutputInterface;
  * Android interactions.  Designing the assignments this way allows
  * you to first learn key 'Java' features without having to beforehand
  * learn the complexities of Android.
- *
  */
-public class Logic 
+public class Logic
        implements LogicInterface {
     /**
      * This is a String to be used in Logging (if/when you decide you
      * need it for debugging).
      */
-    public static final String TAG = 
-        Logic.class.getName();
+    public static final String TAG = Logic.class.getName();
 
-    /*
+    /**
      * This is the variable that stores our OutputInterface instance.
      * <p>
      * This is how we will interact with the User Interface [MainActivity.java].
      * <p>
      * It is called 'out' because it is where we 'out-put' our
      * results. (It is also the 'in-put' from where we get values
-     * from, but it only needs 1 name, and 'out' is good enough)
+     * from, but it only needs 1 name, and 'out' is good enough).
      */
     private OutputInterface mOut;
 
     /**
-     * These are the numeric values that you will receive from the
-     * User Interface and use in your calculations.
-     */
-    private static double mRadius = 0;
-    private static double mLength = 0;
-    private static double mWidth = 0;
-    private static double mHeight = 0;
-
-    /**
      * This is the constructor of this class.
      * <p>
-     * It assigns the passed in [MainActivity] instance
-     * (which implements [OutputInterface]) to 'out'
+     * It assigns the passed in [MainActivity] instance (which
+     * implements [OutputInterface]) to 'out'.
      */
     public Logic(OutputInterface out){
         mOut = out;
@@ -54,142 +43,25 @@ public class Logic
      * This is the method that will (eventually) get called when the
      * on-screen button labeled 'Process...' is pressed.
      */
-    @Override
-    public void process() {
-        // Get which calculation should be computed.  Do not worry
-        // about the specifics of this right now.
-        Shapes shapeForCalculations = mOut.getShape();
-
-        // Store the values returned by the User Interface.
-        mLength = mOut.getLength();
-        mWidth = mOut.getWidth();
-        mHeight = mOut.getHeight();
-        mRadius = mOut.getRadius();
-
-        // Determine which calculation to process right now.  Again,
-        // do not worry about the specifics of how this works for now.
-        switch (shapeForCalculations) {
-            case Box:
-                mOut.print("A " 
-                           + mLength 
-                           + " by " 
-                           + mWidth 
-                           + " by " 
-                           + mHeight 
-                           + " box has a volume of: ");
-                mOut.println("" 
-                             + String.format("%.2f", boxVolume(mLength, mWidth, mHeight)));
-                mOut.println("");
-
-                mOut.print("A " 
-                           + mLength 
-                           + " by " 
-                           + mWidth 
-                           + " by " 
-                           + mHeight 
-                           + " box has a surface area of: ");
-                mOut.println("" 
-                             + String.format("%.2f",
-                                             boxSurfaceArea(mLength, mWidth, mHeight)));
-                mOut.println("");
-                // If you are paying attention, you will notice that
-                // there is no 'break;' here like there is in other
-                // places, meaning that if 'Box' was selected, it will
-                // run the two sets of print statements above and the
-                // two statements below until the 'break;' statement.
-            case Rectangle:
-                mOut.print("A " 
-                           + mLength 
-                           + " by " 
-                           + mWidth 
-                           + " rectangle has a perimeter of: ");
-                mOut.println("" + String.format("%.2f", 
-                                                rectanglePerimeter(mLength, mWidth)));
-                mOut.println("");
-
-                mOut.print("A " 
-                           + mLength 
-                           + " by " 
-                           + mWidth 
-                           + " rectangle has area of: ");
-                mOut.println("" + String.format("%.2f", rectangleArea(mLength, mWidth)));
-                mOut.println("");
-                break;
-            case Sphere:
-                mOut.print("A sphere with radius " + mRadius + " has a volume of: ");
-                mOut.println("" + String.format("%.2f", sphereVolume(mRadius)));
-                mOut.println("");
-
-                mOut.print("A sphere with radius " + mRadius + " has surface area of: ");
-                mOut.println("" + String.format("%.2f", sphereSurfaceArea(mRadius)));
-                mOut.println("");
-                // Same here as with 'Box' above. If 'Sphere' is picked, it will run the
-                // two sets of print statements above and the two below until the 'break;'
-            case Circle:
-                mOut.print("A circle with radius " + mRadius + " has a perimeter of: ");
-                mOut.println("" + String.format("%.2f", circleCircumference(mRadius)));
-                mOut.println("");
-
-                mOut.print("A circle with radius " + mRadius + " has area of: ");
-                mOut.println("" + String.format("%.2f", circleArea(mRadius)) );
-                mOut.println("");
-                break;
-            case Triangle:
-                mOut.print("A right triangle with base " 
-                           + mLength 
-                           + " and height " 
-                           + mWidth + " has a perimeter of: ");
-                mOut.println("" 
-                             + String.format("%.2f", rightTrianglePerimeter(mLength,
-                                                                       mWidth)));
-                mOut.println("");
-
-                mOut.print("A right triangle with base " 
-                           + mLength 
-                           + " and height " 
-                           + mWidth 
-                           + " has area of: ");
-                mOut.println("" 
-                             + String.format("%.2f", rightTriangleArea(mLength,
-                        mWidth)));
-                mOut.println("");
-                break;
-            default:
-                break;
+public void process(int size) {
+//       TODO -- add your code here
+        int a = 0;
+        int count = 0;
+        for (int i = 0; i < size * 2 + 1; i++) {
+            String sym = i % 2 == 1 ? "=" : "-";
+            if (i == 0 || i == size * 2) {
+                mOut.println("+" + repeat("-", size*2) + "+");
+            } else if (i == size) {
+                mOut.println("|<" + repeat(sym, size * 2 - 2) + ">|");
+                a += 2;
+                count += 1;
+            } else {
+                String slash1 = i < size ? "/" : "\\";
+                String slash2 = i < size ? "\\" : "/";
+                a += i < size ? 2 : -2;
+                count += i < size ? 1 : -1;
+                mOut.println("|" + repeat(" ", size-count) + slash1 + repeat(sym, a -2) + slash2 + repeat(" ", size-count) + "|");
+            }
         }
     }
-
-    // TODO -- add your code here
-    public static double rectangleArea(double length, double width) {
-        return width * length;
-    }
-    public static double rectanglePerimeter(double length, double width) {
-        return (width + length) * 2;
-    }
-    public static double  circleArea(double radius) {
-        return radius * radius * Math.PI;
-    }
-    public static double  circleCircumference(double radius) {
-        return radius * 2 * Math.PI;
-    }
-    public static double  rightTriangleArea(double base, double height) {
-        return base * height / 2;
-    }
-    public static double  rightTrianglePerimeter(double base, double height) {
-        double gip = Math.sqrt(height * height + base * base);
-        return gip + base + height;
-    }
-    public static double  boxVolume(double length, double width, double depth) {
-        return width * length * depth;
-    }
-    public static double  boxSurfaceArea(double length, double width, double depth) {
-        return 2 * (width * length + width * depth + depth * length);
-    }
-    public static double  sphereVolume(double radius) {
-        return 4 * Math.PI * radius * radius * radius / 3;
-    }
-    public static double  sphereSurfaceArea(double radius) {
-        return 4 * Math.PI * radius * radius;
-    }
-
 }
